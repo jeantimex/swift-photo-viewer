b8071ee9b5c6515e446ee681b09749df0923c59d
jeantimex <jean.timex@gmail.com>
2017-03-31 10:37:51 -0700

    Initial setup.

---
+++ Pods/Target Support Files/Pods-Swift Photo ViewerUITests/Pods-Swift Photo ViewerUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Swift_Photo_ViewerUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Swift_Photo_ViewerUITestsVersionString[];
+
